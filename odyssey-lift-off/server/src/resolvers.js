78444ae93e8211bb956723fbc863f3f1b5f1355b
octobercold <theminddepth@gmail.com>
2022-09-05 20:45:28 +0100

    complete second part of tutorial

---
+++ odyssey-lift-off/server/src/resolvers.js
@@ -0,0 +1,16 @@
+const TrackAPI = require("./datasources/track-api");
+
+const resolvers = {
+    Query: {
+        tracksForHome: (_, __, { dataSources }) => {
+            return dataSources.trackAPI.getTracksForHome();
+        },
+    },
+    Track: {
+        author: ({ authorId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        },
+    },
+};
+
+module.exports = resolvers;
